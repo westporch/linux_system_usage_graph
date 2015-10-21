ed086bae98c0b6ce655294d6aa7225194239b49f
Westporch <westporch@gmail.com>
2015-10-21 10:45:21 +0900

    제목 및 x,y 축 글씨 출력 문제 수정

--- samples/test.R
+++ samples/test.R
@@ -2,13 +2,12 @@
 #Rscript test.R을 하면 그래프가 생성됨.
 
 #test.R
-x <- readLines('scan.txt')
-y <- c(10, 20, 30, 40, 50, 60, 70) 
+x <- readLines('scan.txt')                                                                                           
+y <- seq(10,70,10)
+#par(mfrow=c(2,3), mar=c(4,4,3,3))
+
 png("test.png")
-plot(x, y, type='o', col='red')
+
+plot(x, y, type='o', col='red', xlab = "", ylab = "") 
 grid()
-
-title(main="title", font.main=5)                                                              
-title(xlab="B", col.lab="black")
-title(ylab="C", col.lab="black") 
-
+title(main="MEMORY (sample)", xlab="Time (s)", ylab="Usage (MB)", font.main=2, cex=2, cex.axis=1.5, cex.sub=1.5, cex.main=2, cex.lab=1.5)
