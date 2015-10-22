b14f8385af56d2c18f8357c53c1661cf753104c7
Westporch <westporch@gmail.com>
2015-10-23 03:28:14 +0900

    메모리 사용량(KB)을 MB로 변환함

--- collect_data.sh
+++ collect_data.sh
@@ -3,14 +3,17 @@
 MemFree_kb=`cat /proc/meminfo | grep MemFree | awk '{print $2}'`
 MemFree_mb=`echo "$MemFree_kb 1024" | awk '{print $1/$2}'`
 Cached_kb=`cat /proc/meminfo | grep ^Cached | awk '{print $2}'`
+Cached_mb=`echo "$Cached_kb 1024" | awk '{print $1/$2}'`
 Active_anon_kb=`cat /proc/meminfo | grep Active\(anon\) | awk '{print $2}'`
 Active_file_kb=`cat /proc/meminfo | grep Active\(file\) | awk '{print $2}'`
-Active=`expr $Active_anon_kb + $Active_file_kb`
+Active_kb=`expr $Active_anon_kb + $Active_file_kb`
+Active_mb=`echo "$Active_kb 1024" | awk '{print $1/$2}'`
 
 echo $MemFree_kb
 echo $MemFree_mb
-echo $Active_anon_kb
-echo $Active_file_kb
-echo $Active
+echo $Cached_kb
+echo $Cached_mb
+echo $Active_kb
+echo $Active_mb
 
 
