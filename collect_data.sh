816a5177713f7ec40981c725f2b0f84bed1d21b6
Westporch <westporch@gmail.com>
2015-10-23 03:06:26 +0900

    /proc/meminfo의 Active(file) 값을 추출함

--- collect_data.sh
+++ collect_data.sh
@@ -3,5 +3,6 @@
 MemFree_kb=`cat /proc/meminfo | grep MemFree | awk '{print $2}'`
 Cached_kb=`cat /proc/meminfo | grep ^Cached | awk '{print $2}'`
 Active_anon_kb=`cat /proc/meminfo | grep Active\(anon\) | awk '{print $2}'`
+Active_file_kb=`cat /proc/meminfo | grep Active\(file\) | awk '{print $2}'`
 
 
