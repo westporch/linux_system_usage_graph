e2af65136e707a0c0b227f843b7f8edb609aef95
Westporch <westporch@gmail.com>
2015-10-23 03:23:12 +0900

    MemFree_mb 계산 기능 추가

--- collect_data.sh
+++ collect_data.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 MemFree_kb=`cat /proc/meminfo | grep MemFree | awk '{print $2}'`
+MemFree_mb=`echo "$MemFree_kb 1024" | awk '{print $1/$2}'`
 Cached_kb=`cat /proc/meminfo | grep ^Cached | awk '{print $2}'`
 Active_anon_kb=`cat /proc/meminfo | grep Active\(anon\) | awk '{print $2}'`
 Active_file_kb=`cat /proc/meminfo | grep Active\(file\) | awk '{print $2}'`
 Active=`expr $Active_anon_kb + $Active_file_kb`
 
+echo $MemFree_kb
+echo $MemFree_mb
 echo $Active_anon_kb
 echo $Active_file_kb
 echo $Active
