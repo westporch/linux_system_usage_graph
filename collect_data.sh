622dcf647590a147b643df17dba89715f2fa90d0
Westporch <westporch@gmail.com>
2015-10-23 03:15:26 +0900

    Active = Active(anon) + Active(file) 계산 기능 추가

--- collect_data.sh
+++ collect_data.sh
@@ -4,5 +4,10 @@ MemFree_kb=`cat /proc/meminfo | grep MemFree | awk '{print $2}'`
 Cached_kb=`cat /proc/meminfo | grep ^Cached | awk '{print $2}'`
 Active_anon_kb=`cat /proc/meminfo | grep Active\(anon\) | awk '{print $2}'`
 Active_file_kb=`cat /proc/meminfo | grep Active\(file\) | awk '{print $2}'`
+Active=`expr $Active_anon_kb + $Active_file_kb`
+
+echo $Active_anon_kb
+echo $Active_file_kb
+echo $Active
 
 
