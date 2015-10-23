c96f395f378f9d75101cd43d56d92a4a77cf7e08
Westporch <westporch@gmail.com>
2015-10-23 11:33:01 +0900

    메모리 사용량 수집 기능 추가

--- collect_data.sh
+++ collect_data.sh
@@ -1,19 +1,49 @@
 #!/bin/bash
 
-MemFree_kb=`cat /proc/meminfo | grep MemFree | awk '{print $2}'`
-MemFree_mb=`echo "$MemFree_kb 1024" | awk '{print $1/$2}'`
-Cached_kb=`cat /proc/meminfo | grep ^Cached | awk '{print $2}'`
-Cached_mb=`echo "$Cached_kb 1024" | awk '{print $1/$2}'`
-Active_anon_kb=`cat /proc/meminfo | grep Active\(anon\) | awk '{print $2}'`
-Active_file_kb=`cat /proc/meminfo | grep Active\(file\) | awk '{print $2}'`
-Active_kb=`expr $Active_anon_kb + $Active_file_kb`
-Active_mb=`echo "$Active_kb 1024" | awk '{print $1/$2}'`
+#HyunGwan Seo
 
-echo $MemFree_kb
-echo $MemFree_mb
-echo $Cached_kb
-echo $Cached_mb
-echo $Active_kb
-echo $Active_mb
+#./collect_data.sh stop -> 데이터 수집을 중지함.
 
+ALL_DATA=all_data.txt
 
+function get_data()
+{
+	MemFree_kb=`cat /proc/meminfo | grep MemFree | awk '{print $2}'`
+	MemFree_mb=`echo "$MemFree_kb 1024" | awk '{print $1/$2}'`
+	Cached_kb=`cat /proc/meminfo | grep ^Cached | awk '{print $2}'`
+	Cached_mb=`echo "$Cached_kb 1024" | awk '{print $1/$2}'`
+	Active_anon_kb=`cat /proc/meminfo | grep Active\(anon\) | awk '{print $2}'`
+	Active_file_kb=`cat /proc/meminfo | grep Active\(file\) | awk '{print $2}'`
+	Active_kb=`expr $Active_anon_kb + $Active_file_kb`
+	Active_mb=`echo "$Active_kb 1024" | awk '{print $1/$2}'`
+
+	WEEK=`date +%W`
+	YEAR=`date +%Y`
+	MONTH=`date +%m`
+	DAY=`date +%d`
+	HOUR=`date +%H`
+	MINUTE=`date +%M`
+	SECOND=`date +%S`
+}
+
+function print_data()
+{
+		for ((;;))
+		do
+			get_data
+			echo "$WEEK,$YEAR,$MONTH,$DAY,$HOUR,$MINUTE,$SECOND,$MemFree_mb,$Active_mb,$Cached_mb" >> collect_data.txt
+			sleep 1m	# 데이터 수집 주기 설정 ex) 30s -> 30초, 10m -> 10분, 1h -> 1시간
+		done
+}
+
+function init_document()
+{
+	echo "Week,Year,Month,Day,Hour,Minute,Second,MemFree,Active,Cached" > collect_data.txt
+}
+
+if [ "$1" == "stop" ];then
+	pkill collect_data.sh	# 데이터 수집을 중지함
+else
+	init_document
+	print_data
+fi
