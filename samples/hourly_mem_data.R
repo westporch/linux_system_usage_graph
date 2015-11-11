916807bc268aec6855d6684df48643433de4a00e
Westporch <westporch@gmail.com>
2015-11-11 17:27:11 +0900

    반복문에서 작업 중

--- samples/hourly_mem_data.R
+++ samples/hourly_mem_data.R
@@ -2,9 +2,24 @@ library(sqldf)
 mem <- read.csv('mem_statistics.csv')
 hourly_arr <- sqldf('select Hour from mem where Year=2015 and Month=11 and Day=11 group by Hour')
 
-hourly_arr
+#hourly_arr
 
-NROW(hourly_arr) # 배열의 전체 길이를 구함.
+num_of_hour <- NROW(hourly_arr) # 배열의 전체 길이를 구함.
+
+for (idx in 1:num_of_hour)
+{
+	idx <- hourly_arr[idx,]
+
+	hour_data <- sprintf("select MemFree from mem where Year=2015 and Month=11 and Day=11 and Hour=%s", idx)
+	get_hour_data <- sqldf(hour_data)
+
+	print(idx)
+	print(get_hour_data)
+	print("=====end======")
+}
+
+
+
 
 #hourly_arr[1,]
 #hourly_arr[2,]
