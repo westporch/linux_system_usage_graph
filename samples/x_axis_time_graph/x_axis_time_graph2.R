1b05be929ca2fd0ed1e90c10940f88d98c2d6e9b
Westporch <westporch@gmail.com>
2015-11-12 17:44:04 +0900

    그래프 3개를 A4 용지 사이즈에 출력함

--- samples/x_axis_time_graph/x_axis_time_graph2.R
+++ samples/x_axis_time_graph/x_axis_time_graph2.R
@@ -1,12 +1,14 @@
 #Hyeongwan Seo
 
-library(ggplot2)
+library(gridExtra) # 라이브러리를 로드하지 못하면 install.packages("gridExtra")를 해야함.
+library(ggplot2) #라이브러리를 로드하지 못하면 install.packages("ggplot2")를 해야함.
 
 dataset <- read.csv(file="mem_statistics.csv", as.is=TRUE)
 
 dataset$Time <- strptime(format="%d-%m-%Y %H:%M:%S %z", dataset$Timestamp, tz="GMT")
 
-p1 <- ggplot( dataset, aes( Time, MemFree ) ) +
+#그래프1
+plot1 <- ggplot( dataset, aes( Time, MemFree ) ) +
 			  geom_point(color="black") + 
 			  geom_line(colour="red") + 
 			  scale_x_datetime() + 
@@ -14,4 +16,24 @@ p1 <- ggplot( dataset, aes( Time, MemFree ) ) +
 			  ggtitle("[Memory] Memfree - by a day") +
 			  theme(plot.title = element_text(color="#666666", face="bold", size=16, hjust=0.5)) +
   			  theme(axis.title = element_text(color="#666666", face="bold", size=14)) 
-ggsave(filename="test.png")
+#그래프2
+plot2 <- ggplot( dataset, aes( Time, MemFree ) ) +
+			  geom_point(color="black") + 
+			  geom_line(colour="red") + 
+			  scale_x_datetime() + 
+			  labs(x="Time (HH:MM)", y="MemFree (MB)") + 
+			  ggtitle("[Memory] Memfree - by a day") +
+			  theme(plot.title = element_text(color="#666666", face="bold", size=16, hjust=0.5)) +
+  			  theme(axis.title = element_text(color="#666666", face="bold", size=14)) 
+#그래프3
+plot3 <- ggplot( dataset, aes( Time, MemFree ) ) +
+			  geom_point(color="black") + 
+			  geom_line(colour="red") + 
+			  scale_x_datetime() + 
+			  labs(x="Time (HH:MM)", y="MemFree (MB)") + 
+			  ggtitle("[Memory] Memfree - by a day") +
+			  theme(plot.title = element_text(color="#666666", face="bold", size=16, hjust=0.5)) +
+  			  theme(axis.title = element_text(color="#666666", face="bold", size=14)) 
+
+png(filename="test.png", width=595, height=842, unit="px")
+grid.arrange(plot1, plot2, plot3, ncol=1, nrow=3)
