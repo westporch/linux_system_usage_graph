3231500f639cd0e7c034400c80bd984744e1e76b
Westporch <westporch@gmail.com>
2015-11-13 13:09:56 +0900

    Grid의 linetype을 수정함

--- samples/x_axis_time_graph/x_axis_time_graph2.R
+++ samples/x_axis_time_graph/x_axis_time_graph2.R
@@ -16,8 +16,10 @@ plot1 <- ggplot( dataset, aes( Time, MemFree ) ) +
 			  ggtitle("[Memory] Memfree - by a day") +
 			  theme(plot.title = element_text(color="#666666", face="bold", size=12, hjust=0.5)) +
   			  theme(axis.title = element_text(color="#666666", face="bold", size=8, hjust=1)) +
-			  theme(panel.grid.major = element_line(colour = "#969696", size=0.3)) +
-			  theme(panel.grid.minor = element_line(colour = "black", size=0.05))
+			  theme(panel.grid.major = element_line(colour = "#969696", size=0.3, linetype='F1')) +
+			  theme(panel.grid.minor = element_line(colour = "#969696", size=0.4, linetype='dotted')) +
+              theme(panel.border = element_rect(colour = "#aaaaaa", fill=NA, size=1))
+
 
 #그래프2 (Active)
 plot2 <- ggplot( dataset, aes( Time, Active ) ) +
@@ -28,8 +30,9 @@ plot2 <- ggplot( dataset, aes( Time, Active ) ) +
 			  ggtitle("[Memory] Active - by a day") +
 			  theme(plot.title = element_text(color="#666666", face="bold", size=12, hjust=0.5)) +
   			  theme(axis.title = element_text(color="#666666", face="bold", size=8, hjust=1)) +
-			  theme(panel.grid.major = element_line(colour = "#969696", size=0.3)) +
-			  theme(panel.grid.minor = element_line(colour = "black", size=0.05))
+			  theme(panel.grid.major = element_line(colour = "#969696", size=0.3, linetype='F1')) +
+			  theme(panel.grid.minor = element_line(colour = "#969696", size=0.4, linetype='dotted')) +
+              theme(panel.border = element_rect(colour = "#aaaaaa", fill=NA, size=1))
 
 #그래프3 (Cached)
 plot3 <- ggplot( dataset, aes( Time, Cached ) ) +
@@ -40,8 +43,9 @@ plot3 <- ggplot( dataset, aes( Time, Cached ) ) +
 			  ggtitle("[Memory] Cached - by a day") +
 			  theme(plot.title = element_text(color="#666666", face="bold", size=12, hjust=0.5)) +
   			  theme(axis.title = element_text(color="#666666", face="bold", size=8, hjust=1)) +
-			  theme(panel.grid.major = element_line(colour = "#969696", size=0.3)) +
-			  theme(panel.grid.minor = element_line(colour = "black", size=0.05))
+			  theme(panel.grid.major = element_line(colour = "#969696", size=0.3, linetype='F1')) +
+			  theme(panel.grid.minor = element_line(colour = "#969696", size=0.4, linetype='dotted')) +
+              theme(panel.border = element_rect(colour = "#aaaaaa", fill=NA, size=1))
 
 png(filename="test.png", width=595, height=842, unit="px", res=150)
 grid.arrange(plot1, plot2, plot3, ncol=1, nrow=3)
