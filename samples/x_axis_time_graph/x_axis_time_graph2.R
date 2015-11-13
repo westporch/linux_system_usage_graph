d3d37a3b38b2c23501790e7096d3ea725acb979c
Westporch <westporch@gmail.com>
2015-11-13 13:51:42 +0900

    그래프 출력 화면 크기 수정

--- samples/x_axis_time_graph/x_axis_time_graph2.R
+++ samples/x_axis_time_graph/x_axis_time_graph2.R
@@ -20,13 +20,12 @@ plot1 <- ggplot( dataset, aes( Time, MemFree ) ) +
 			  theme(panel.grid.minor = element_line(colour = "#969696", size=0.4, linetype='dotted')) +
               theme(panel.border = element_rect(colour = "#aaaaaa", fill=NA, size=1))
 
-
 #그래프2 (Active)
 plot2 <- ggplot( dataset, aes( Time, Active ) ) +
 			  geom_point(color="#FF9999") + 
 			  geom_line(colour="#FF9999") + 
 			  scale_x_datetime() + 
-			  labs(x="Time (HH:MM)", y="Cached (MB)") + 
+			  labs(x="Time (HH:MM)", y="Active (MB)") + 
 			  ggtitle("[Memory] Active - by a day") +
 			  theme(plot.title = element_text(color="#666666", face="bold", size=12, hjust=0.5)) +
   			  theme(axis.title = element_text(color="#666666", face="bold", size=8, hjust=1)) +
@@ -39,7 +38,7 @@ plot3 <- ggplot( dataset, aes( Time, Cached ) ) +
 			  geom_point(color="steelblue2") + 
 			  geom_line(colour="steelblue2") + 
 			  scale_x_datetime() + 
-			  labs(x="Time (HH:MM)", y="Active (MB)") + 
+			  labs(x="Time (HH:MM)", y="Cached (MB)") + 
 			  ggtitle("[Memory] Cached - by a day") +
 			  theme(plot.title = element_text(color="#666666", face="bold", size=12, hjust=0.5)) +
   			  theme(axis.title = element_text(color="#666666", face="bold", size=8, hjust=1)) +
@@ -47,7 +46,8 @@ plot3 <- ggplot( dataset, aes( Time, Cached ) ) +
 			  theme(panel.grid.minor = element_line(colour = "#969696", size=0.4, linetype='dotted')) +
               theme(panel.border = element_rect(colour = "#aaaaaa", fill=NA, size=1))
 
-#png(filename="test.png", width=595, height=842, unit="px", res=150)
+#png(filename="test.png", width=842, height=794, unit="px", res=170)
 png(filename="test.png", width=842, height=794, unit="px", res=150)
-#grid.arrange(plot1, plot2, ncol=1, nrow=2)
 grid.arrange(plot1, plot2, plot3, ncol=1, nrow=3)
+
+#png(filename="test.png", width=595, height=842, unit="px", res=150)
