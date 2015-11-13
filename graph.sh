a42bbc1d984dcabe3f99feb54474deb4f7127101
Westporch <westporch@gmail.com>
2015-11-13 17:16:12 +0900

    case문 수정

--- graph.sh
+++ graph.sh
@@ -2,7 +2,7 @@
 
 #Hyeongwan Seo
 
-function daily()
+function daily() # 삭제하지마 나중에 다이어로그 그릴때 참고할 예정
 {
 	TMP_FILE=/tmp/input.box.tmp.$$
 
@@ -26,10 +26,9 @@ function daily()
 	esac
 }
 
-
-cmd=(dialog --checklist "Memory usage graph" 15 40 4)
+cmd=(dialog --checklist "시스템 사용량을 그래프로 보여줍니다." 15 40 4)
 options=(1 "메모리 사용량 그래프" on    # any option can be set to default to "on"
-         2 "CPU 사용량 그래프 (임시)" off
+         2 "CPU 사용량 그래프 (구현x)" off
          3 "기능 추가 예정" off
          4 "기능 추가 예정" off)
 
@@ -38,16 +37,12 @@ clear
 for choice in $choices
 do
 	case $choice in
-		1) echo "1이 선택됨"
-			menu_idx=1
-			#daily;;
-			Rscript memory_graph.R;;
-		2) echo "2" 
-			menu_idx=2;;
-		3) echo "3" 
-			menu_idx=3;;
-		4) echo "4" 
-			menu_idx=4;;
+		1)	#daily;;
+			Rscript memory_graph.R
+			echo "메모리 사용량 그래프를 완성했습니다.";;
+		2) echo "2";;
+		3) echo "3";; 
+		4) echo "4";; 
 		*) echo "유효하지 않은 숫자!" ;;
 	esac
 done
