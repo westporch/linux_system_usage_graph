16f2d728dcc7a9eda66a428fa44e2c7797c3e52b
Westporch <westporch@gmail.com>
2015-11-16 21:45:54 +0900

    xy 평면에 Memfree, Active, Cached를 나타냄

---
+++ United_memory_graph.R
@@ -0,0 +1,30 @@
+#Hyeongwan Seo
+
+library(gridExtra) # 라이브러리를 로드하지 못하면 install.packages("gridExtra")를 해야함.
+library(ggplot2) #라이브러리를 로드하지 못하면 install.packages("ggplot2")를 해야함.
+
+dataset <- read.csv(file="mem_statistics.csv", as.is=TRUE)
+
+dataset$Time <- strptime(format="%d-%m-%Y %H:%M:%S %z", dataset$Timestamp, tz="GMT")
+
+# y축의 max 값을 계산한다                                  
+y_Memfree <- dataset[,9]
+y_Active <- dataset[,10]
+y_Cached <- dataset[,11]
+y_max <- max(y_Memfree, y_Active, y_Cached)
+
+dataset$Time <- strptime(format="%d-%m-%Y %H:%M:%S %z", dataset$Timestamp, tz="GMT")
+
+plot <- ggplot( dataset, aes( Time, MB ), legend=TRUE ) + 
+            scale_y_continuous(breaks = seq(0, y_max, by = 100)) +
+            ggtitle("Memory usage") +
+            theme(panel.grid.major = element_line(colour = "#969696", size=0.3, linetype='F1')) +
+            theme(panel.grid.minor = element_line(colour = "#969696", size=0.4, linetype='dotted')) +
+            theme(panel.border = element_rect(colour = "#aaaaaa", fill=NA, size=1)) +
+            geom_line(colour="#6ED746", aes(Time, y = y_Memfree )) +
+            geom_line(colour="#FF9999", aes(Time, y = y_Active )) +   
+            geom_line(colour="steelblue2", aes(Time, y = y_Cached)) +
+            scale_colour_manual(values=c("green","red","blue")) # 작동 안함 (확인 필요)
+
+png(filename="Memory_usage_graph.png", width=24000, height=22300, unit="px", res=2900)
+grid.arrange(plot, ncol=1, nrow=1)
