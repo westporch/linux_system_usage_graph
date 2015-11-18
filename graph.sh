e833ef7316f5b0a3e624719d5206b1c4bf693e7e
Westporch <westporch@gmail.com>
2015-11-18 15:06:23 +0900

    메뉴 항목에서 CPU 사용량을 그래프로 그리도록 함

--- graph.sh
+++ graph.sh
@@ -28,7 +28,7 @@ function daily() # 삭제하지마 나중에 다이어로그 그릴때 참고할
 
 cmd=(dialog --checklist "시스템 사용량을 그래프로 보여줍니다." 15 40 4)
 options=(1 "메모리 사용량 그래프" on    # any option can be set to default to "on"
-         2 "CPU 사용량 그래프 (구현x)" off
+         2 "CPU 사용량 그래프" off
          3 "기능 추가 예정" off
          4 "기능 추가 예정" off)
 
@@ -39,9 +39,11 @@ do
 	case $choice in
 		1)	#daily;;
 			#Rscript memory_graph.R
-			Rscript United_memory_graph.R
+			Rscript Memory/United_memory_graph.R
 			echo -e "\e[1;32m메모리 사용량 그래프(Memory_usage_graph.png)를 완성했습니다.\e[0m";;
-		2) echo "2";;
+
+		2)  Rscript CPU/United_CPU_graph.R
+			echo -e "\e[1;32mCPU 사용량 그래프(CPU_usage_graph.png)를 완성했습니다.\e[0m";;
 		3) echo "3";; 
 		4) echo "4";; 
 		*) echo "유효하지 않은 숫자!" ;;
